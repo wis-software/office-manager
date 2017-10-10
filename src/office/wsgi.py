545c8ab6c41cf07dbe5140f3d2b70a73c90d642f
ilya <i.masliev@wis.eu.com>
2017-10-10 17:11:56 +0300

    Remove dotenv django app, add src as volume

--- src/office/wsgi.py
+++ src/office/wsgi.py
@@ -9,10 +9,7 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 
 import os
 
-import dotenv
 from django.core.wsgi import get_wsgi_application
-
-dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "office.settings")
 
