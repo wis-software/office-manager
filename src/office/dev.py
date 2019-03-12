7ca685ecb36d8fe98436bcca8e3704619cbd076f
Evgeny Golyshev <eugulixes@gmail.com>
2019-03-12 19:00:18 +0300

    Use .env file to override settings

---
+++ src/office/dev.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+
+from pathlib import Path
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+from office.settings import *
