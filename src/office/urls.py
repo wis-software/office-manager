09b9ba367ec06cabe99904e13acd49d1f6b0fafd
Daria Buksha <d.buksha@wis.eu.com>
2019-03-13 08:19:00 +0300

    fix(auth): added verify token, updated endpoints

--- src/office/urls.py
+++ src/office/urls.py
@@ -5,14 +5,16 @@ from django.conf.urls import include, url
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView,
 )
 
 from office.views import DRFAuthenticatedGraphQLView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^token/$', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    url(r'^token/refresh/$', TokenRefreshView.as_view(), name='token_refresh'),
+    url(r'^api-token-auth/$', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    url(r'^api-token-refresh/$', TokenRefreshView.as_view(), name='token_refresh'),
+    url(r'^api-token-verify/$', TokenVerifyView.as_view(), name='token_verify'),
     url(r'^graphql/$', DRFAuthenticatedGraphQLView.as_view(graphiql=True)),
 ]
 
