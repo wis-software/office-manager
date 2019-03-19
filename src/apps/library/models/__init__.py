c6915137a39bd60159c2dedcefc6dcbc38ecdc39
ilya masliev <ilya.ma@ezscratch.com>
2019-03-19 16:24:04 +0300

    add author model to init

--- src/apps/library/models/__init__.py
+++ src/apps/library/models/__init__.py
@@ -2,3 +2,4 @@ from apps.library.models.book import Book
 from apps.library.models.offer import Offer
 from apps.library.models.holder import Holder
 from apps.library.models.tag import Tag
+from apps.library.models.author import Author
