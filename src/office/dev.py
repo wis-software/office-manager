e7097a7b5100d4af588dd1c60c0640796607e62b
ilya masliev <ilya.ma@ezscratch.com>
2019-03-19 16:39:42 +0300

    sync to master

--- src/office/dev.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-
-load_dotenv(verbose=True)
-
-from pathlib import Path
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
-
-from office.settings import *
