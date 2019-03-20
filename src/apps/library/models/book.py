6b1d27801775b0b5d3cd097cf50c5d0dd8823fec
ilya masliev <ilya.ma@ezscratch.com>
2019-03-20 12:15:25 +0300

    add m2m relation from book to publisher

--- src/apps/library/models/book.py
+++ src/apps/library/models/book.py
@@ -9,7 +9,8 @@ __all__ = [
 class Book(models.Model):
     name = models.CharField(_('name'), max_length=1024)
     author = models.CharField(_('author'), max_length=256)
-    publisher = models.CharField(_('publisher'), max_length=256)
+    publishers = models.ManyToManyField('library.Publisher', verbose_name=_('publishers'),
+                                  blank=True, related_name='books')
     description = models.TextField(_('description'), default='', blank=True)
     tags = models.ManyToManyField('library.Tag', verbose_name=_('tags'),
                                   blank=True, related_name='books')
