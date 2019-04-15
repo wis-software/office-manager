2b020b353e009c644cecef79a2d030a15d47a8b0
Ilya Masliev <ilamasliev@Ilyas-MacBook-Air.local>
2019-04-15 15:06:57 +0300

    add admin filters for library

--- apps/library/admin.py
+++ apps/library/admin.py
@@ -3,9 +3,31 @@ from django.contrib import admin
 from apps.library import models
 
 
-admin.site.register(models.Book)
-admin.site.register(models.Holder)
-admin.site.register(models.Offer)
-admin.site.register(models.Tag)
-admin.site.register(models.Author)
-admin.site.register(models.Publisher)
+@admin.register(models.Book)
+class BookAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'description', 'tag__name')
+
+
+@admin.register(models.Holder)
+class HolderAdmin(admin.ModelAdmin):
+    search_fields = ('employee__first_name', 'book__name')
+
+
+@admin.register(models.Offer)
+class OfferAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'description')
+
+
+@admin.register(models.Tag)
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+
+@admin.register(models.Author)
+class AuthorAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'about')
+
+
+@admin.register(models.Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'description')
